a9f1777e17435d7b36043f79732a22280d916442
Space-Belt <dngurwn@gmail.com>
2025-04-07 21:16:11 +0900

    feat: 앱실행 및 함수 바로실행, double float 확인

---
+++ app/src/main/java/com/example/firstkotlinpracticeapp/Basics.kt
@@ -0,0 +1,12 @@
+package com.example.firstkotlinpracticeapp
+
+fun first() {
+    println("Hello World")
+}
+
+fun floatDouble() {
+    var pi = 3.14f
+    println(pi)
+    pi = 3.1415926535f
+    println(pi)
+}
