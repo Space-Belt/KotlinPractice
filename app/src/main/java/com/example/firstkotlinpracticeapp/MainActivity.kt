9d6b296347cc43ab684047e9b9898d964365f57b
Space-Belt <dngurwn@gmail.com>
2025-04-10 20:17:52 +0900

    feat: 상태값, 토스트, 반복문, 함수 활용 간단 보물찾기게임

--- app/src/main/java/com/example/firstkotlinpracticeapp/MainActivity.kt
+++ app/src/main/java/com/example/firstkotlinpracticeapp/MainActivity.kt
@@ -1,17 +1,27 @@
 package com.example.firstkotlinpracticeapp
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.firstkotlinpracticeapp.ui.theme.FirstKotlinPracticeAppTheme
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,28 +30,92 @@ class MainActivity : ComponentActivity() {
         setContent {
             FirstKotlinPracticeAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Joo",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    CaptainGame()
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hi $name!",
-        modifier = modifier
-    )
-}
+    @Composable
+    fun CaptainGame() {
+        val directions = listOf("동", "서", "남", "북")
+        val treasuresFound = remember { mutableStateOf(0) }
+        val direction = remember { mutableStateOf("북") }
+        val toastMessage = remember {mutableStateOf("")}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FirstKotlinPracticeAppTheme {
-        Greeting("Android")
+        val stormOrTreasure = remember { mutableStateOf("") }
+
+        val context = LocalContext.current
+
+        fun showToast(dir: String) {
+            direction.value = dir
+            if(Random.nextBoolean()) {
+                treasuresFound.value += 1
+                toastMessage.value = ""
+            } else {
+                toastMessage.value = "못"
+                stormOrTreasure.value = "폭풍이다!!"
+            }
+            Toast.makeText(context, "${direction.value}쪽에서 보물을 ${toastMessage.value}찾았습니다", Toast.LENGTH_SHORT).show()
+        }
+
+        @Composable
+        fun DirectionButton(direction: String) {
+            Button(
+                onClick = {
+                    showToast(direction)
+                },
+
+            ) {
+                Text("${direction}쪽으로")
+            }
+        }
+
+        @Composable
+        fun DirectionButtons() { // 방향 리스트
+
+            Row(modifier = Modifier.fillMaxWidth()) {
+                directions.forEach { direction ->
+                    DirectionButton(direction)  // 공통 버튼 컴포저블
+                }
+            }
+        }
+
+        Column(modifier = Modifier.padding(top = 50.dp)) {
+            Text(text = "보물 발견: ${treasuresFound.value}")
+            Text(text = "보물 방향: ${direction.value}")
+            Row {
+                Button(onClick = {
+                    showToast("동")
+                }){
+                    Text("동쪽으로")
+                }
+                Button(onClick = {
+                    showToast("서")
+                }){
+                    Text("서쪽으로")
+                }
+                Button(onClick = {
+                    showToast("남")
+                }){
+                    Text("남쪽으로")
+                }
+                Button(onClick = {
+                    showToast("북")
+                }){
+                    Text("북쪽으로")
+                }
+            }
+            Row {
+                DirectionButtons()
+
+            }
+            Text(text = stormOrTreasure.value)
+        }
     }
+
+
 }
+
+
+
