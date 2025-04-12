b36195f0395a8050480ad06b00e93b86c29a4cd7
Space-Belt <dngurwn@gmail.com>
2025-04-12 15:52:32 +0900

    feat: 카운터 상속 학습

---
+++ app/src/main/java/com/example/firstkotlinpracticeapp/Inheritance.kt
@@ -0,0 +1,9 @@
+package com.example.firstkotlinpracticeapp
+
+fun main() {
+    val obj1 = InheritanceBaseClass()
+    obj1.coreValues()
+
+    val obj2 = InheritanceSecondClass()
+    obj2.coreValues()
+}
---
+++ app/src/main/java/com/example/firstkotlinpracticeapp/InheritanceBaseClass.kt
@@ -0,0 +1,7 @@
+package com.example.firstkotlinpracticeapp
+
+open class InheritanceBaseClass {
+    fun coreValues() {
+        println("Core values of house Baseclass")
+    }
+}
