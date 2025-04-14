cf4aebf0fa5e4002614e33d1840f9050726319bd
Space-Belt <dngurwn@gmail.com>
2025-04-14 12:57:05 +0900

    feat: MVVM에서 데이터를 관리하고 접근하는 레포지토리 학습

--- app/src/main/java/com/example/firstkotlinpracticeapp/CounterViewModelMvvm.kt
+++ app/src/main/java/com/example/firstkotlinpracticeapp/CounterViewModelMvvm.kt
@@ -4,18 +4,21 @@ import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 
-class CounterViewModelMvvm : ViewModel() {
-    private val _count = mutableStateOf(0)
+class CounterViewModelMvvm() : ViewModel() {
+    private val repository: CounterRepository = CounterRepository()
+    private val _count = mutableStateOf(repository.getCounter().count)
 
     // _count 외부로 노출 시키기 위한 불변상태의 count
     val count : MutableState<Int> = _count
 
     fun increment() {
-        _count.value++
+        repository.incrementCounter()
+        _count.value = repository.getCounter().count
     }
 
     fun decrement() {
-        _count.value--
+        repository.decrementCounter()
+        _count.value = repository.getCounter().count
     }
 
 }
