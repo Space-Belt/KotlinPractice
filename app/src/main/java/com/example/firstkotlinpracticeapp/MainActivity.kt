d5dd33b18bece43b0c191faee65be4bf827e83a6
Space-Belt <dngurwn@gmail.com>
2025-04-15 20:37:12 +0900

    feat: 네비게이션 학습

--- app/src/main/java/com/example/firstkotlinpracticeapp/MainActivity.kt
+++ app/src/main/java/com/example/firstkotlinpracticeapp/MainActivity.kt
@@ -1,51 +1,63 @@
 package com.example.firstkotlinpracticeapp
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.firstkotlinpracticeapp.ui.theme.FirstKotlinPracticeAppTheme
-import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            val viewModel: CounterViewModelMvvm = viewModel()
             FirstKotlinPracticeAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    TheCounterApp(viewModel)
+//                    RecipeScreen()
+                    MyApp()
                 }
             }
         }
     }
 }
 
+//@Serializable
+//object Profile
 
+@Composable
+fun MyApp() {
+    val navContoller = rememberNavController()
+    NavHost(navController = navContoller, startDestination = "firstNavigationScreen") {
+        composable("firstNavigationScreen"){
+            FirstScreen {
+                navContoller.navigate("SecondNavigationScreen")
+            }
+        }
+        composable("secondNavigationScreen"){
+            SecondScreen {
+                navContoller.navigate("FirstNavigationScreen")
+            }
+        }
 
+    }
+}
+
+//@Serializable
+//object Profile
+//@Serializable
+//object FriendsList
+//
+//val navController = rememberNavController()
+//
+//NavHost(navController = navController, startDestination = Profile) {
+//    composable<Profile> { ProfileScreen( /* ... */ ) }
+//    composable<FriendsList> { FriendsListScreen( /* ... */ ) }
+//    // Add more destinations similarly.
+//}
